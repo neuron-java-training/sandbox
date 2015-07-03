733ec1c3fa5c4342e8ce75582a8139b6fcd2c66d
Tököli Zoltán <zoli0506@gmail.com>
2015-07-03 13:47:08 +0200

    add

--- project/app/src/main/java/hu/neuron/java/project/app/Test.java
+++
@@ -1,9 +0,0 @@
-package hu.neuron.java.project.app;
-
-public interface Test {
-	String getCollectionName();
-	long init();
-	long sort();
-	long get(int a);
-	long delete(int first, int second);
-}
