811006521af03e82ccf5386230970a6b43550c29
Tököli Zoltán <zoli0506@gmail.com>
2015-07-06 12:05:22 +0200

    add

---
+++ project/core/src/main/java/hu/neuron/java/project/core/TestingResults.java
@@ -0,0 +1,59 @@
+package hu.neuron.java.project.core;
+
+public class TestingResults {
+	public TestingResults() {
+		super();
+	}
+	long sortTime;
+	long getTime;
+	long deleteTime;
+	long initTime;
+	String collectionName;
+	public long getSortTime() {
+		return sortTime;
+	}
+	public void setSortTime(long sortTime) {
+		this.sortTime = sortTime;
+	}
+	public long getGetTime() {
+		return getTime;
+	}
+	public void setGetTime(long getTime) {
+		this.getTime = getTime;
+	}
+	public long getDeleteTime() {
+		return deleteTime;
+	}
+	public void setDeleteTime(long deleteTime) {
+		this.deleteTime = deleteTime;
+	}
+	public long getInitTime() {
+		return initTime;
+	}
+	public void setInitTime(long initTime) {
+		this.initTime = initTime;
+	}
+	
+	public void display() {
+		System.out.println("Collection: " + collectionName);
+		System.out.println("Creation time: " + initTime );
+		System.out.println("Get time: " + getTime);
+		System.out.println("Sort time: " + sortTime);
+		System.out.println("Deletion time: " + deleteTime);
+		
+}
+	
+	public String getCollectionName() {
+		return collectionName;
+	}
+	public void setCollectionName(String collectionName) {
+		this.collectionName = collectionName;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "CollectionName " + this.getCollectionName() + " DeletionTime " + this.getDeleteTime() + " Sort Time " + this.getDeleteTime() + " getTime " + this.getTime + " initTime " + this.getInitTime(); 
+	}
+	
+	
+}
